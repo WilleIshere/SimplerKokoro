cd422f5c07ac0618c7ffd35267a64185e1e5d4e4
WilleIshere <wille2008080808@gmail.com>
2025-10-22 16:31:15 +0200

    Fixed models_dir documentation

--- examples/custom_models_dir_example.py
+++ examples/custom_models_dir_example.py
@@ -1,10 +1,9 @@
 from Simpler_Kokoro import SimplerKokoro
 
-sk = SimplerKokoro()
+sk = SimplerKokoro(models_dir="Folder-to-put-models-in",) # Here is the models_dir parameter
 voices = sk.list_voices()
 
 sk.generate(
-    models_dir="Folder-to-put-models-in",
     text="Thats a cool model directory.",
     voice=voices[1]['name'],
     output_path="fast_output.wav",
