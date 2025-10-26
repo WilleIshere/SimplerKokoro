4643331df565e1efa225e07d09d56c6280ddc9ce
WilleIshere <wille2008080808@gmail.com>
2025-10-26 14:27:00 +0100

    Update text in subtitles_example.py for clarity and unify string quotes

--- examples/subtitles_example.py
+++ examples/subtitles_example.py
@@ -11,9 +11,9 @@ selected_voice = voices[0]
 
 # Generate speech
 sk.generate(
-    text="Hello, this is a test of the Simpler Kokoro voice synthesis.", # Text to generate
+    text='Hello, this will generate a subtitles.srt file along with output.wav', # Text to generate
     voice=selected_voice.name, # Grab the name from the selected voice
-    output_path="output.wav", # Select the output path
+    output_path='output.wav', # Select the output path
     write_subtitles=True, # Enable subtitle generation
     subtitles_path='subtitles.srt', # (optional) Specify the subtitle .srt filename
     subtitles_word_level=True # (optional) Enable word level timestamps
